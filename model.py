4d7dbe40a807cb24116f64680d0ce514203d6717
YussafZarifi <t44ysf@gmail.com>
2026-09-09 18:20:24 -0400

    Refactor ConvolutionalNetwork architecture
    Refactored the ConvolutionalNetwork class to simplify the architecture by removing redundant fully connected layers and adding an adaptive average pooling layer.

--- model.py
+++ model.py
@@ -4,46 +4,29 @@ import torch.nn.functional as F
 
 
 class ConvolutionalNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.Conv1 = nn.Conv2d(3, 6, 3, 1)
-        self.Conv2 = nn.Conv2d(6, 12, 3, 1)
-        self.Conv3 = nn.Conv2d(12, 24, 3, 1)
+  def __init__(self, num_classes=2):
+    super().__init__()
+    self.Conv1 = nn.Conv2d(3, 6, 3, 1)
+    self.Conv2 = nn.Conv2d(6, 12, 3, 1)
+    self.Conv3 = nn.Conv2d(12, 24, 3, 1)
+    self.gap = nn.AdaptiveAvgPool2d(1)   # shrinks 26x26x24 -> 1x1x24
+    self.fc1 = nn.Linear(24, 64)
+    self.fc2 = nn.Linear(64, num_classes)
 
-        # Fully Connected Layer
-        self.fc1 = nn.Linear(26 * 26 * 24, 10000)
-        self.fc2 = nn.Linear(10000, 7000)
-        self.fc3 = nn.Linear(7000, 4000)
-        self.fc4 = nn.Linear(4000, 1000)
-        self.fc5 = nn.Linear(1000, 100)
-        self.fc6 = nn.Linear(100, 2)
+  def forward(self, X):
+    X = F.relu(self.Conv1(X))
+    X = F.max_pool2d(X, 2, 2)
+    X = F.relu(self.Conv2(X))
+    X = F.max_pool2d(X, 2, 2)
+    X = F.relu(self.Conv3(X))
+    X = F.max_pool2d(X, 2, 2)
 
-    def forward(self, X):
-        # First Pass
-        X = F.relu(self.Conv1(X))
-        X = F.max_pool2d(X, 2, 2)
+    X = self.gap(X)
+    X = X.view(X.size(0), -1)   # flatten to (batch, 24)
 
-        # Second Pass
-        X = F.relu(self.Conv2(X))
-        X = F.max_pool2d(X, 2, 2)
-
-        # Third Pass
-        X = F.relu(self.Conv3(X))
-        X = F.max_pool2d(X, 2, 2)
-
-        # Flatten
-        X = X.view(-1, 24 * 26 * 26)
-
-        # Fully connected layers
-        X = F.relu(self.fc1(X))
-        X = F.relu(self.fc2(X))
-        X = F.relu(self.fc3(X))
-        X = F.relu(self.fc4(X))
-        X = F.relu(self.fc5(X))
-        X = self.fc6(X)
-
-        # You trained with log_softmax at the end
-        return F.log_softmax(X, dim=1)
+    X = F.relu(self.fc1(X))
+    X = self.fc2(X)
+    return F.log_softmax(X, dim=1)
 
 
 def load_model(checkpoint_path: str, device: torch.device):
